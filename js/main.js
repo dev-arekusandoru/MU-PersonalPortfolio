d98994239b7168a2e44e9d92369d51ee3c9d8e19
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2021-12-07 23:44:16 -0500

    added commentary

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const setTitleDate = () => {
-
-    console.log("this works");
+    //get the date info
     const d = new Date();
 
+    //string to be displayed in the header
     let dateString = "";
 
+    //stores the weekday, month, day of month, and year values from d
     let weekday = d.getDay();
     let month = d.getMonth();
     let day = d.getDate();
     let year = d.getFullYear();
 
+    //based on the value of weekday, add the correct day of the week to the dateString
     switch(weekday) {
         case 0:
             dateString += "Sunday";
@@ -34,8 +36,10 @@ const setTitleDate = () => {
             break;
     }
 
+    //add some formatting
     dateString += ", ";
 
+    //based on the value of month, add the correct month to the dateString
     switch(month) {
         case 0:
             dateString += "January";
@@ -75,11 +79,14 @@ const setTitleDate = () => {
             break;
     }
 
+    //formatting, and add day/year
     dateString += " " + day + ", " + year;
 
-
+    //set the value in the html
     document.querySelector('#date').innerHTML = dateString;
 
 };
 
+
+//run it
 setTitleDate();
